8e6ed4e7749a67176c43fba8eb803a2fa0927d3e
nadiduno <nadiduno@gmail.com>
2025-01-10 22:42:45 -0300

    Setup project - sprint boot

---
+++ src/main/java/com/edu/apiEducation/ApiEducationApplication.java
@@ -0,0 +1,13 @@
+package com.edu.apiEducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/apiEducation/ApiEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.apiEducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
