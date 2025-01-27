bbd1537213f7bc0430db490350482d073f50a1b3
nadiduno <nadiduno@gmail.com>
2025-01-26 23:04:18 -0300

    Delete dependecies

--- src/main/java/com/edu/apiEducation/controller/TrilhaController.java
+++ src/main/java/com/edu/apiEducation/controller/TrilhaController.java
@@ -7,28 +7,20 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.edu.apiEducation.trilha.CadastrarDadosTrilha;
 import com.edu.apiEducation.trilha.DadosListagemTrilha;
-import com.edu.apiEducation.trilha.ResourceNotFoundException;
 import com.edu.apiEducation.trilha.Trilha;
 import com.edu.apiEducation.trilha.TrilhaRepository;
-import com.edu.apiEducation.trilha.TrilhaService;
 
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
-import lombok.Data;
 
 @RestController
 @RequestMapping("/trilha")
